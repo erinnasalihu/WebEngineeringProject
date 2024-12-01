d766adf42d867191a4e0f276eac3c7305dd11568
erinnasalihu <erinasalihu@hotmail.com>
2024-12-01 02:06:16 +0100

    HomePage Function Added

---
+++ HomePage/Home/home.js
@@ -0,0 +1,7 @@
+function toggleNavbar() {
+    document.querySelector('.navbar').classList.toggle('active');
+}
+
+function toggleProfileMenu() {
+    document.querySelector('.profile-menu').classList.toggle('active');
+}
