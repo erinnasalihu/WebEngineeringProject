f1a00904265b08273ec9b10620f509fa432d8207
AnduenaSyla <anduenasylaa@gmail.com>
2025-02-02 19:08:34 +0100

    minor update

--- Home/Sweet n Savory/savory.php
+++ Home/Sweet n Savory/savory.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="savory.css">
+    <link rel="stylesheet" href="../../common/navigation.css">
     <title>Savory</title>
 </head>
 <body>
